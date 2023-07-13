43224a8a3294ccb182c7b7b5e7f7cc0ea5b50fad
Aegean <todddharni@gmail.com>
2023-07-12 21:33:44 -0600

    Restructured prompts and added in files for jest

---
+++ shapes.js
@@ -0,0 +1,17 @@
+class Shape {
+
+}
+
+class Circle extends Shape {
+
+}
+
+class Triangle extends Shape {
+
+}
+
+class Square extends Shape {
+
+}
+
+module.exports = {Shape, Circle, Triangle, Square}
